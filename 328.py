ae1610f869f893171d70d8291f590aac10a93420
xpandi <xpandi@qq.com>
2018-12-22 17:07:55 -0500

    ListNode_oddEvenList

---
+++ 328.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def printList(self, head):
+        s = ""
+        while head:
+            s = s + "->" + str(head.val)
+            head = head.next
+        print(s)
+
+    def oddEvenList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        if not head or not head.next:
+            return head
+        odd = head
+        even = head.next
+        evenh = even
+        while even and even.next:
+            odd.next = even.next
+            odd = odd.next
+            even.next = odd.next
+            even = even.next
+        odd.next = evenh
+        return head
